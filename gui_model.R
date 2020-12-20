d362f385f60d45d63ed7b71dbdb25c97c5060a44
Elżbieta Jowik <43819024+ejowik@users.noreply.github.com>
2020-12-20 11:13:48 +0100

    Add files via upload

--- gui_model.R
+++ gui_model.R
@@ -1,66 +1,139 @@
 library(shiny)
 library(shinyjs)
 library(dplyr)
+library(DT)
 library(httr)
 timeout(1000)
 source("model_from_random_points_combined.R")
 
 ui <- fluidPage(
-  numericInput("min_lat_deg", label = "Minimum latitude (degrees)", 
-               value = 45, min = 0, max = 90),
-  numericInput("min_lat_min", label = "Minimum latitude (minutes)", 
-               value = 30, min = 0, max = 60),
-  numericInput("max_lat_deg", label = "Maximum lattitude (degrees)", 
-               value = 64, min = 0, max = 90),
-  numericInput("max_lat_min", label = "Maximum lattitude (minutes)", 
-               value = 12, min = 0, max = 60),
-  numericInput("min_lon_deg", label = "Minimum longitude (degrees)", 
-               value = 32, min = 0, max = 90),
-  numericInput("min_lon_min", label = "Minimum longitude (minutes)", 
-               value = 17, min = 0, max = 60),
-  numericInput("max_lon_deg", label = "Maximum longitude (degrees)", 
-               value = 56, min = 0, max = 90),
-  numericInput("max_lon_min", label = "Maximum longitude (minutes)", 
-               value = 0, min = 0, max = 60),
+  theme = shinytheme('flatly'),
+  titlePanel("Criterions "),
+  sidebarPanel(
+    radioButtons(inputId = "dataChoice", label = "Specify the location preferences",
+                 choices = c("Regions","Coordinates"), inline = TRUE),
+    
+    fluidRow(selectInput(inputId = "regions", label = "Regions selection",
+                         choices = list(Polska = c("dolnoslaskie" = "D",
+                                                   "kujawsko-pomorskie" = "C",
+                                                   "lubelskie" = "L",
+                                                   "lubuskie" = "F",
+                                                   "lodzkie" = "E",
+                                                   "malopolskie" = "K",
+                                                   "mazowieckie" = "W",
+                                                   "opolskie" = "O",
+                                                   "podkarpackie" = "R",
+                                                   "podlaskie" = "B",
+                                                   "pomorskie" = "G",
+                                                   "slaskie" = "S",
+                                                   "swietokrzyskie" = "T",
+                                                   "warminsko-mazurskie" = "N",
+                                                   "wielkopolskie"= "P",
+                                                   "zachodniopomorskie" = "Z")), selected = NULL, multiple = TRUE)),
+    
+    h5("Coordinates selection"),
+    numericInput("min_lat_deg", label = "Minimum latitude (degrees)", 
+                 value = 45.5, min = 0, max = 90),
+    numericInput("max_lat_deg", label = "Maximum lattitude (degrees)", 
+                 value = 64.34, min = 0, max = 90),
+    numericInput("min_lon_deg", label = "Minimum longitude (degrees)", 
+                 value = 32.57, min = 0, max = 90),
+    numericInput("max_lon_deg", label = "Maximum longitude (degrees)", 
+                 value = 56.33, min = 0, max = 90),
+    
+    verbatimTextOutput("value"),
+    
+    fluidRow(selectInput(inputId = "resources", label = "Resources proximity preferences",
+                         choices = list("Rivers" = "rivers", "Crops" = "crops", "Forests" = "forests", "Motorways" = "motorways"), 
+                         selected = NULL, multiple = TRUE)),
+    actionButton("btn", "Predict")
+    
+  ),
+  mainPanel(
+    radioButtons('format', 'Document format', c('HTML', 'Word'), inline = TRUE),
+    downloadButton('downloadReport'),
+    br(),
+    br(),
+    h4("Model predictions"),
+    DT::dataTableOutput('table')
+  ),
+  useShinyjs()
   
-  verbatimTextOutput("value"),
-  tableOutput('table'),
-  useShinyjs(),
-  
-  actionButton("btn", "Predict")
   
 )
 server <- function(input, output) {
   
+  observeEvent(input$dataChoice, {
+    if(input$dataChoice == "Coordinates"){
+      enable('min_lat_deg') 
+      enable('max_lat_deg')
+      enable('min_lon_deg')
+      enable('max_lon_deg')
+      disable('regions')
+    }
+    else{
+      disable('min_lat_deg') 
+      disable('max_lat_deg')
+      disable('min_lon_deg')
+      disable('max_lon_deg')
+      enable('regions')
+    }})
+  
   numbers <- reactive({
     shiny::validate(
       need(is.numeric(input$min_lon_deg), "Please input a number"),
-      need(is.numeric(input$min_lon_min), "Please input a number"),
       need(is.numeric(input$max_lon_deg), "Please input a number"),
-      need(is.numeric(input$max_lon_min), "Please input a number"),
       need(is.numeric(input$min_lat_deg), "Please input a number"),
-      need(is.numeric(input$min_lat_min), "Please input a number"),
-      need(is.numeric(input$max_lat_deg), "Please input a number"),
-      need(is.numeric(input$max_lat_min), "Please input a number")
+      need(is.numeric(input$max_lat_deg), "Please input a number")
     )
   })
   output$value <- renderPrint({ numbers() })
-
   
-
+  
+  
   # Call Onclick
-   renderTab <- function(){
-     xmin <- input$min_lat_deg + input$min_lat_min * 0.01
-     xmax <- input$max_lat_deg + input$max_lat_min * 0.01
-     ymin <- input$min_lon_deg + input$min_lon_min * 0.01
-     ymax <- input$max_lon_deg + input$max_lon_min * 0.01
-     
-     result <- predict_from_area(xmin, ymin, xmax, ymax, n = 10)
-     prediction <- head(result[order(as.vector(result$score), decreasing=TRUE),],5)
-     output$table <- renderTable(prediction)
-
-   }
-   onclick("btn", renderTab)
+  renderTab <- function(){
+    xmin <- input$min_lat_deg
+    xmax <- input$max_lat_deg 
+    ymin <- input$min_lon_deg  
+    ymax <- input$max_lon_deg
+    
+    # result <- predict_from_area(xmin, ymin, xmax, ymax, n = 10)
+    # prediction <- head(result[order(as.vector(result$score), decreasing=TRUE),],5)
+    
+    # testowo wstawilam tutaj dowolna ramke, bo moje RStudio nie wspolpracuje z modelem
+    # na potrzeby projektu wystarczy odkomentowac to co jest wyzej
+    prediction <- head(iris) 
+    output$table <- DT::renderDataTable(prediction)
+    
+  }
+  onclick("btn", renderTab)
+  
+  output$downloadReport <- downloadHandler(
+    filename = function() {
+      paste('Report', sep = '.', switch(
+        input$format, HTML = 'html', Word = 'docx'
+      ))
+    },
+    
+    # Trzeba sie zastanowic do wrzucic do zawartosci raportu
+    content = function(file) {
+      src <- normalizePath('report.Rmd')
+      
+      # temporarily switch to the temp dir, in case you do not have write
+      # permission to the current working directory
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      
+      library(rmarkdown)
+      out <- render('report.Rmd', switch(
+        input$format,
+        HTML = html_document(), Word = word_document()
+      ))
+      file.rename(out, file)
+    }
+  )
+  
 }   
 
 shinyApp(ui = ui, server = server)
